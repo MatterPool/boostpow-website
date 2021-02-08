fc9715186d8732177c7acf470f1029d66c5745cc
Dean <dean@bitping.com>
2021-02-09 10:30:59 +1100

    favicon

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: "https://graph.boostpow.com/api/v1/main/boost",
   scripts: [
-    "http://localhost:4000/publish.js"
-    // "https://boostpow-publish.web.app/publish.js"
+    // "http://localhost:4000/publish.js"
+    "https://boostpow-publish.web.app/publish.js"
   ]
 };
