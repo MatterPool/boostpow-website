319410bb36374bcbc7f44473e8098af214f096fe
Daniel Krawisz <daniel.krawisz@thingobjectentity.net>
2021-02-15 01:20:51 -0600

    Attempt to make drop-down box work but failed.

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { APIService, TimeSelectOptions } from 'src/app/_services/api.service';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faLess } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-content',
@@ -17,12 +18,13 @@ export class ContentComponent implements OnInit {
   searchForm: FormGroup;
   subscriptions: Subscription[] = [];
   searching = false;
+  searched = false;
 
   ranks: any;
 
-  id:string;
+  id: string;
 
-  constructor(private api: APIService, private fb: FormBuilder, private route: ActivatedRoute) {
+  constructor(private api:APIService, private fb: FormBuilder, private route: ActivatedRoute, private router:Router) {
     this.id = this.route.snapshot.params['id'];
     let timeframe = this.route.snapshot.queryParams['timeframe'] || 86400*14;
     if(!this.timeSelectOptions.find(o => { return o.value == timeframe})){
@@ -31,7 +33,7 @@ export class ContentComponent implements OnInit {
     this.searchForm = this.fb.group({
       timeframe: [timeframe]
     });
-    this.getContent(this.id, timeframe);
+    this.getRanks(this.id, timeframe);
   }
 
   ngOnInit(): void {
@@ -44,11 +46,14 @@ export class ContentComponent implements OnInit {
     ];
   }
 
-  async getContent(id: string, timeframe: number) {
+  async getRanks(id:string, timeframe:number) {
+    this.searching = true;
+    this.router.navigate(['c/' + id], { queryParams: { timeframe: timeframe } });
     try {
       await this.api.getRanksData(id, timeframe);
     } catch(e) {
       console.error(e);
     }
+    this.searched = true;
   }
 }
